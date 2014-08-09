b20632db019c7f80023e69e3da213722f1a282c2
Pathak <getsantanupathak@gmail.com>
2014-08-10 01:02:20 +0530

    Update plot4 for Exploratory Data Analysis Course Project Week1

--- plot4.R
+++ plot4.R
@@ -2,6 +2,7 @@ electric_power_consumption <- read.table("household_power_consumption.txt",heade
 subset_epc <- electric_power_consumption[(as.Date(electric_power_consumption$Date,"%d/%m/%Y") >= as.Date('01/02/2007',"%d/%m/%Y") & as.Date(electric_power_consumption$Date,"%d/%m/%Y") <= as.Date('02/02/2007',"%d/%m/%Y")),]
 subset_epc_merged <- within(subset_epc, DateTime <- paste(Date,Time,sep=" "))
 png(filename="plot4.png",width=480,height=480, units = "px")
+par(mfrow=c(2,2))
 plot(strptime(subset_epc_merged$DateTime,"%d/%m/%Y %H:%M:%S"),subset_epc_merged$Global_active_power,type="l", xlab="DateTime",ylab="Global Active Power")
 plot(strptime(subset_epc_merged$DateTime,"%d/%m/%Y %H:%M:%S"),subset_epc_merged$Voltage,type="l", xlab="DateTime",ylab="Voltage")
 plot(strptime(subset_epc_merged$DateTime,"%d/%m/%Y %H:%M:%S"),subset_epc_merged$Sub_metering_1,type="l", xlab="Time",ylab="Energy Sub Metering")
